f119f22387bc59f1060d3c16ac72bb970bdd6398
Toshihiro Takehara <nouphet.ratanavong@gmail.com>
2012-09-09 13:00:24 +0900

    Update html/modules/myalbum/xoops_version.php
    version number up

--- html/modules/myalbum/xoops_version.php
+++ html/modules/myalbum/xoops_version.php
@@ -32,7 +32,7 @@ if( ! preg_match( '/^(\D+)(\d*)$/' , $mydirname , $regs ) ) echo ( "invalid dirn
 $mydirnumber = $regs[2] === '' ? '' : intval( $regs[2] ) ;
 
 $modversion['name'] = _ALBM_MYALBUM_NAME . $mydirnumber ;
-$modversion['version'] = 2.89 ;
+$modversion['version'] = 2.90 ;
 $modversion['description'] = _ALBM_MYALBUM_DESC;
 $modversion['author'] = "GIJ=CHECKMATE<br />PEAK Corp.(http://www.peak.ne.jp/)" ;
 $modversion['credits'] = "Original: Daniel Branco<br />(http://bluetopia.homeip.net)<br />Kazumi Ono<br />(http://www.mywebaddons.com/)<br />The XOOPS Project" ;
